0116fc03c8b59622ffdf0f97f5135020ac880379
Lubin Babu <lubinnbabu@gmail.com>
2019-05-29 17:46:49 -0400

    Add project files.

---
+++ 200390262_1/Controllers/CarController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _200390262_1.Models;
+namespace _200390262_1.Controllers
+{
+    public class CarController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(carlist);
+        }
+
+
+        List<Car> carlist = new List<Car>()
+        {
+            new Car {id=1, Brand ="Toyota",Model ="corolA", Type="compact", Year=2019},
+            new Car {id=2, Brand ="Toyota",Model ="Land cruiser", Type="compact", Year=2009},
+            new Car {id=3, Brand ="Toyota",Model ="Prado", Type="compact", Year=1919}
+        };
+       public ActionResult Detail(int? id)
+        {
+            
+            if (id==null || id > carlist.Count)
+            {
+                return Content("Invalid car id");
+            }
+            var ind = Convert.ToInt32(id) - 1;
+            var car = carlist[ind];
+            return View(car);
+        }
+    }
+}
---
+++ 200390262_1/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _200390262_1.Models{
+
+    public class Car
+{
+    public int id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string Type { get; set; }
+    public int Year { get; set; }
+}
+}
